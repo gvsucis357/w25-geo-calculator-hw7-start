e4b847a74a62f1ebfcd65d82bc42ce8b88b389db
Jonathan Engelsma <jengelsma@gmail.com>
2025-01-30 16:46:18 -0500

    Initial Commit

---
+++ GeoCalculator-SwiftUI/GeoCalculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoCalculator_SwiftUIApp.swift
+//  GeoCalculator-SwiftUI
+//
+//  Created by Jonathan Engelsma on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct GeoCalculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeoCalculator-SwiftUITests/GeoCalculator_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  GeoCalculator_SwiftUITests.swift
+//  GeoCalculator-SwiftUITests
+//
+//  Created by Jonathan Engelsma on 1/30/25.
+//
+
+import Testing
+@testable import GeoCalculator_SwiftUI
+
+struct GeoCalculator_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
